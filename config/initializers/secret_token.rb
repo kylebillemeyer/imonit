c30d64e02fec6e111cf2a1bca0806bef3486efa6
Kyle Billemeyer <billemeyer.k@gmail.com>
2013-04-03 12:45:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imonit::Application.config.secret_token = 'd6591148e366fc8a5f9cbb134e7639c306a56044cb1116599392e67e694bd6e77ab6f51613e04a537c072f72d7ccdf227da7d1e4c8beb27e3211cf6f1d92c186'
