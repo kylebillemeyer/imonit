5abf389a1294656be3347f79c34f56ead7082dcd
Kyle Billemeyer <billemeyer.k@gmail.com>
2013-04-12 12:09:05 -0400

    add pg to prod

--- app/controllers/my_group_controller.rb
+++ app/controllers/my_group_controller.rb
@@ -3,5 +3,6 @@ class MyGroupController < ActionController::Base
   def index
   	@group = Group.find(params[:group_id])
     @members = User.where(:group_id => params[:group_id])
+    @trackings = Tracking.includes(:item).where(:group_id => params[:group_id])
   end
 end
--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -2,5 +2,5 @@ class Tracking < ActiveRecord::Base
   attr_accessible :count, :group_id, :item_id, :tenant_on_it, :threshold, :total_estimated_cost
   belongs_to :item
   belongs_to :group
-  has_one :user
+  has_one :user, :foreign_key => :tenant_on_it
 end
